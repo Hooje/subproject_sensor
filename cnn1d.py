6447fe9b23f2ce008bc14ff1f0c2fb475f521f10
Hooje <a9584627155@gmail.com>
2022-05-07 11:07:08 +0800

    first commit

---
+++ cnn1d.py
@@ -0,0 +1,17 @@
+import pickle
+import numpy as np
+#from keras.models import Sequential
+import torch 
+'''
+model_m = Sequential()
+model_m.add(Reshape((TIME_PERIODS, num_sensors), input_shape=(input_shape,)))
+model_m.add(Conv1D(100, 10, activation='relu', input_shape=(TIME_PERIODS, num_sensors)))
+model_m.add(Conv1D(100, 10, activation='relu'))
+model_m.add(MaxPooling1D(3))
+model_m.add(Conv1D(160, 10, activation='relu'))
+model_m.add(Conv1D(160, 10, activation='relu'))
+model_m.add(GlobalAveragePooling1D())
+model_m.add(Dropout(0.5))
+model_m.add(Dense(num_classes, activation='softmax'))
+print(model_m.summary())
+'''
